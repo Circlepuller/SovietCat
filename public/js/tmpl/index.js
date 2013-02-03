76b3ac5c4c92e2cc9e125a36b7ddca327f6f918f
Riku Rouvila <riku.rouvila@gmail.com>
2013-02-04 00:59:58 +0200

    gitignore for compiled files

--- public/js/tmpl/index.js
+++
@@ -1,8 +0,0 @@
-this["JST"] = this["JST"] || {};
-
-this["JST"]["src/handlebars/index.hbs"] = function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers; data = data || {};
-  
-
-
-  return "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n  <title></title>\r\n</head>\r\n<body>\r\n\r\n</body>\r\n</html>";};
